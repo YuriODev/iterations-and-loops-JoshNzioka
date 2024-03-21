c7ff53ef27117070c105f402466e08a501e6d00e
JoshNzioka <161600531+JoshNzioka@users.noreply.github.com>
2024-03-21 09:44:47 +0000

    Attempt 1

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,1 +1,4 @@
-# Your solution to Exercise 4
+# Your solution to Exercise 
+num=int(input())
+for count in range(1,num+1):
+  print(count,"#"*count)
--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,1 +1,4 @@
 # Your solution to Exercise 6
+num=int(input())
+for count in range(1,num+1):
+  print("*"*count)
